02875fb84d2ddd66aa110cd7c1bf2d794024d1bd
MathieuCrosnier <mat.crosnier@gmail.com>
2023-01-02 20:29:17 +0100

    Améliorations de l'API

--- API/access.py
+++ API/access.py
@@ -3,50 +3,21 @@ from fastapi.security import OAuth2PasswordBearer , OAuth2PasswordRequestForm
 from jwt import encode , decode , PyJWTError
 from typing import Optional
 from pydantic import BaseModel
-from datetime import datetime , timedelta
+from datetime import datetime , timezone , timedelta
+from sqlalchemy.orm import Session
+from database import start_session , current_user , add_to_users_table , Users
 
 router = APIRouter(tags = ["Access"])
-
-class User(BaseModel):
-    '''
-	Format pour la gestion des utilisateurs.
-	'''
-    username : str
-    password : str
-    is_admin : bool
-
-users = []
-
-admin = User(username = "Mathieu" , password = "Crosnier" , is_admin = True)
 
 oauth2 = OAuth2PasswordBearer(tokenUrl = "token")
 secret_key = "9290a6c64b338dcb7cc17afe83310e284e976670d07580799b35f86ba0bab74a"
 algorithm = "HS256"
 token_expiration = 1
 
-def existing_user(username : str):
-    result = list(filter(lambda x : x.username == username , users))
-    if result == []:
-        return None
-    else:
-        return result[0]
-
-def is_existing_user(username : str):
-    result = existing_user(username) 
-    if result is None:
-        return False
-    else:
-        return True
-
-def save_user(user : User):
-    users.append(user)
-
-save_user(admin)
-
-def generate_token(user : User):
+def generate_token(user : Users):
     dic = {"username" : user.username ,
            "rights" : "Administrator" if user.is_admin else "Standard" ,
-           "exp" : datetime.utcnow() + timedelta(minutes = token_expiration)}
+           "exp" : datetime.now(timezone.utc) + timedelta(minutes = token_expiration)}
     encoded_jwt = encode(dic , secret_key , algorithm = algorithm)
     return encoded_jwt
 
@@ -62,11 +33,11 @@ def decode_token(token : str = Depends(oauth2)):
     return {"username" : username , "rights" : rights}
 
 @router.post("/token" , name = "Generate token")
-async def token(credentials : OAuth2PasswordRequestForm = Depends()):
+async def token(credentials : OAuth2PasswordRequestForm = Depends() , session = Depends(start_session)):
     username = credentials.username
     password = credentials.password
-    user = existing_user(username)
-    if (not is_existing_user(username)) or (is_existing_user(username) & (password != user.password)):
+    user = current_user(username = username , session = session)
+    if (user is None) or ((user is not None) & (password != user.password)):
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED ,
             detail = "Incorrect username or password" ,
@@ -76,21 +47,23 @@ async def token(credentials : OAuth2PasswordRequestForm = Depends()):
     return {"access_token": token, "token_type": "bearer"}
 
 @router.post("/signup" , name = "Create an account")
-async def signup(username : str , password : str):
+async def signup(username : str , password : str , session : Session = Depends(start_session)):
     if (username is None) or (password is None):
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED ,
             detail = "Username or password can't be empty"
         )
-    if not existing_user(username):
-        user = User(username = username , password = password , is_admin = False)
-        users.append(user)
+    user = current_user(username = username , session = session)
+    if user is not None:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED ,
+            detail = "The username already exists"
+        )
+    if user is None:
+        user = Users(username = username , password = password , registered_date = datetime.now(timezone.utc))
+        add_to_users_table(user = user , session = session)
     return "Your account has been created"
 
 @router.get("/user" , name = "Get user information")
 async def user(token : str = Depends(oauth2)):
     return decode_token(token)
-
-@router.get("/users" , name = "Return list of users")
-async def get_users():
-    return users
---
+++ API/database.py
@@ -0,0 +1,56 @@
+from sqlalchemy import select , insert , inspect , Table , Column , Float , Integer , String , DateTime , Boolean , create_engine , text
+from sqlalchemy.orm import sessionmaker , Session
+from sqlalchemy.ext.declarative import declarative_base
+import pymysql
+from datetime import datetime, timezone
+from fastapi import Depends
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = "users"
+
+    username = Column(String(100) , primary_key = True , nullable = False)
+    password = Column(String(100) , nullable = False)
+    is_admin = Column(Boolean , nullable = False , server_default = text("0"))
+    registered_date = Column(DateTime , nullable = False)
+
+class Logs(Base):
+    __tablename__ = "logs"
+
+    username = Column(String(100) , primary_key = True , nullable = False)
+    log_info = Column(String(100) , nullable = False)
+    log_date = Column(DateTime , nullable = False)
+
+class Predictions(Base):
+    __tablename__ = "predictions"
+
+    username = Column(String(100) , primary_key = True , nullable = False)
+    home_team = Column(String(100) , nullable = False)
+    away_team = Column(String(100) , nullable = False)
+    game_date = Column(DateTime , nullable = False)
+    home_odd = Column(Float , nullable = False)
+    draw_odd = Column(Float , nullable = False)
+    away_odd = Column(Float , nullable = False)
+    prediction_date = Column(DateTime , nullable = False)
+
+engine = create_engine("mysql+pymysql://Mathieu:A4xpgru+@localhost/project")
+
+if not inspect(engine).has_table("users") :
+    Base.metadata.create_all(bind = engine)
+
+def start_session():
+    sm = sessionmaker(engine)
+    session = sm()   
+    try:
+        yield session
+    finally:
+        session.close()
+
+def current_user(username : str , session : Session):
+    statement = select(Users).filter_by(username = username)
+    return session.scalars(statement).first()
+
+def add_to_users_table(user : Users , session : Session):
+    session.add(Users(username = user.username , password = user.password , registered_date = datetime.now(timezone.utc)))
+    session.commit()
